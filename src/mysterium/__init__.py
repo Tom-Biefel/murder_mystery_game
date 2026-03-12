4a3bfa1980a9bcc6d442d8142d1788537950e6f5
Tom Biefel <tom.biefel@student.ie.edu>
2026-03-12 19:18:36 +0100

    __init__

--- src/mysterium/__init__.py
+++ src/mysterium/__init__.py
@@ -1,13 +1,3 @@
-"""
-mysterium – Digital version of the murder mystery game CLUE.
-
-Public API:
-    Game        – Main game engine. Instantiate to start a new game.
-    CaseFile    – The sealed solution (suspect, weapon, room).
-    build_mansion   – Build and wire the 11-room mansion dict.
-    validate_name   – Validate a player name against the regex pattern.
-"""
-
 __version__ = "0.1.1"
 __description__ = "Digital version of the murder mystery game CLUE."
 
